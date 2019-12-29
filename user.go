dfcfe10191f346b55e308d1530a0697bf76fb48a
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2019-12-29 06:27:44 +0300

    :sparkles: Update User Informations

--- user.go
+++ user.go
@@ -3,8 +3,14 @@ package deezer
 type UserService service
 
 type User struct {
-	ID        int    `json:"id"`
-	Name      string `json:"name"`
-	Tracklist string `json:"tracklist"`
-	Type      string `json:"type"`
+	ID            int    `json:"id"`
+	Name          string `json:"name"`
+	Link          string `json:"link"`
+	Picture       string `json:"picture"`
+	PictureSmall  string `json:"picture_small"`
+	PictureMedium string `json:"picture_medium"`
+	PictureBig    string `json:"picture_big"`
+	PictureXL     string `json:"picture_xl"`
+	Tracklist     string `json:"tracklist"`
+	Type          string `json:"type"`
 }
