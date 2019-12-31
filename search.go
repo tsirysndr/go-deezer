20eb1a68bf1e2d3a7cf8fd40034ab1f896ff4b29
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2019-12-31 11:48:02 +0300

    :sparkles: Add search song

--- search.go
+++ search.go
@@ -1,3 +1,17 @@
 package deezer
 
 type SearchService service
+
+type SearchParams struct {
+	Q string `url:"q"`
+}
+
+func (s *SearchService) Get(text string) (*Tracks, error) {
+	var err error
+	params := SearchParams{
+		Q: text,
+	}
+	tracks := new(Tracks)
+	s.client.base.Get("search").QueryStruct(params).Receive(tracks, err)
+	return tracks, err
+}
